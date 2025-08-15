3221978986e62b1044b2e8de494a901599050a64
Ryan Goree <goree.ryan@gmail.com>
2025-08-15 17:54:08 -0500

    Switch App to named export

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "#/ui/App";
+import { App } from "#/ui/App";
 import "#/ui/styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
