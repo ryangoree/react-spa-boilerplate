eb3d3d98b28f584ba2500e92f3da711fdde197e2
Ryan Goree <goree.ryan@gmail.com>
2026-05-07 15:08:54 -0500

    bump deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   base: process.env.VITE_BASE_PATH ?? "/",
+  resolve: {
+    tsconfigPaths: true,
+  },
   test: {
     environment: "jsdom",
     coverage: {
